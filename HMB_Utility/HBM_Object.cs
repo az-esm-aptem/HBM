768f0ea60e81363b7666e6e9c3f2712e65add03a
Артем Горяинов <agoryainov@ntcsphere.com>
2019-11-20 10:53:45 +0300

    First commit

---
+++ HMB_Utility/HBM_Object.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Hbm.Api.Common;
+using Hbm.Api.Common.Messaging;
+using Hbm.Api.Common.Exceptions;
+using Hbm.Api.Common.Entities;
+using Hbm.Api.Common.Entities.Problems;
+using Hbm.Api.Common.Entities.Connectors;
+using Hbm.Api.Common.Entities.Channels;
+using Hbm.Api.Common.Entities.Signals;
+using Hbm.Api.Common.Entities.Filters;
+using Hbm.Api.Common.Entities.ConnectionInfos;
+using Hbm.Api.Common.Enums;
+using Hbm.Api.Scan;
+
+namespace HMB_Utility
+{
+    class HBM_Object
+    {
+        DaqEnvironment _daqEnvironment; //main object to work 
+        DaqMeasurement _daqMeasurement; //main object to measurment
+        Device _device;  //device to use
+        List<Signal> _signalsToMeasure; //list of signals to continuous measurment
+        //private delegate void DeviceEventHandler(DeviceEventArgs e); //event handler
+        //MessageBroker _mesBroker;
+
+        public HBM_Object()
+        {
+            _daqEnvironment = DaqEnvironment.GetInstance();
+            _daqMeasurement = new DaqMeasurement();
+        }
+
+        public void Search()
+        {
+
+        }
+    }
+}
